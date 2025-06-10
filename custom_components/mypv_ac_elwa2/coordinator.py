15f284b9a382c3ff68c2799e4f354d51ea94ea78
yniverz <88168412+yniverz@users.noreply.github.com>
2025-06-10 15:50:36 -0700

    Refactor: Remove AsyncModbusTcpClient and update write_target method to use HTTP requests Update: Bump version to 0.4.0 and remove pymodbus requirement from manifest

--- custom_components/mypv_ac_elwa2/coordinator.py
+++ custom_components/mypv_ac_elwa2/coordinator.py
@@ -1,7 +1,6 @@
 from datetime import timedelta
 import asyncio
 import logging
-from pymodbus.client import AsyncModbusTcpClient
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 import aiohttp
 
@@ -17,6 +16,12 @@ async def fetch_json(url):
             response.raise_for_status()  # Optional: raise an error for bad status codes
             data = await response.json()
             return data
+        
+async def send_get(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            response.raise_for_status()  # Optional: raise an error for bad status codes
+            return await response.text()
 
 class ElwaCoordinator(DataUpdateCoordinator):
     def __init__(self, hass, host, scan_sec, resend_sec):
@@ -27,10 +32,9 @@ class ElwaCoordinator(DataUpdateCoordinator):
             name=f"elwa2_{host}",
             update_interval=timedelta(seconds=scan_sec),
         )
-        self._client = AsyncModbusTcpClient(host=host, port=DEFAULT_PORT)
         self._last_target = 0
         self._resend_sec = resend_sec
-        # schedule resend loop
+        
         hass.loop.create_task(self._resend_loop())
 
     async def _async_update_data(self):
@@ -46,7 +50,6 @@ class ElwaCoordinator(DataUpdateCoordinator):
         except Exception as e:
             _LOGGER.error("HTTP/JSON Error from %s: %s", self.host, e)
         
-
         return {
             "temperature": temp_raw * 0.1,
             "current_power": power,
@@ -55,8 +58,7 @@ class ElwaCoordinator(DataUpdateCoordinator):
     async def write_target(self, watts: int):
         watts = max(0, min(MAX_W, watts))
         self._last_target = watts
-        await self._client.connect()
-        await self._client.write_register(address=POWER_SET_REG, value=watts, slave=1)
+        send_get(f"http://{self.host}/control.html?power={watts}")
 
     async def _resend_loop(self):
         """Periodically resend non-zero target to overcome the ELWA timeout."""
